6413ba6c401a49446efe73dbe40305fa77f2926c
George Lin <giolin@google.com>
2026-03-12 16:01:53 +0000

    Rename CustomizationPickerActivity2 (2/3)
    This is to remove the 2 suffix
    
    Test: NA
    Bug: 492169990
    Flag: EXEMPT PURE_REFACTOR
    Change-Id: Id3286c61824b4c7c43bbb57cf14d265ca046fde0

--- src/com/android/customization/module/ThemePickerInjector.kt
+++ src/com/android/customization/module/ThemePickerInjector.kt
@@ -36,7 +36,7 @@ import com.android.wallpaper.picker.category.wrapper.WallpaperCategoryWrapper
 import com.android.wallpaper.picker.customization.data.content.WallpaperClient
 import com.android.wallpaper.picker.customization.data.repository.WallpaperColorsRepository
 import com.android.wallpaper.picker.customization.domain.interactor.WallpaperInteractor
-import com.android.wallpaper.picker.customization.ui.CustomizationPickerActivity2
+import com.android.wallpaper.picker.customization.ui.CustomizationPickerActivity
 import com.android.wallpaper.picker.di.modules.MainDispatcher
 import com.android.wallpaper.system.UiModeManagerWrapper
 import com.android.wallpaper.util.DisplayUtils
@@ -90,7 +90,7 @@ constructor(
 
     override fun getDeepLinkRedirectIntent(context: Context, uri: Uri): Intent {
         val intent = Intent()
-        intent.setClass(context, CustomizationPickerActivity2::class.java)
+        intent.setClass(context, CustomizationPickerActivity::class.java)
         intent.data = uri
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         return intent
